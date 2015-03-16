0f119cd9fc1946a34ada9c9ce5a58ef407fd4f3f
yroffin <yroffin@item-72766>
2015-03-16 23:51:46 +0100

    Init

--- client/public_html/js/controllers.js
+++ client/public_html/js/controllers.js
@@ -18,181 +18,195 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).controller('BootstrapCtrl', [ '$rootScope', '$window', 'moduleServices', function($scope, $window, modulesServices) {
-	/**
-	 * bootstrap
-	 */
-	$scope.load = function() {
-		$scope.theme = "b";
-		$scope.inventoryItem = {
-			href : ''
-		};
-		$scope.modules = {};
-	}
+angular.module('myApp.controllers', []).controller('BootstrapCtrl', ['$rootScope', '$window', 'moduleServices', function ($scope, $window, modulesServices) {
+        /**
+         * bootstrap
+         */
+        $scope.load = function () {
+            $scope.theme = "b";
+            $scope.inventoryItem = {
+                href: ''
+            };
+            $scope.modules = {};
+        }
 
         /**
-	 * node render
-	 */
-	$scope.render = function(clear, s, nodes) {
-            if(clear) s.clear();
+         * node render
+         */
+        $scope.render = function (clear, s, nodes) {
+            /**
+             * clear it if requested
+             */
+            if (clear) {
+                s.clear();
+            }
+ 
             /**
              * render all this nodes
              */
-            nodes.forEach( function(node) {
+            nodes.forEach(function (node) {
                 console.log(node)
                 var nodeCircle = s.circle(150, 150, 100);
                 var nodeTitle = s.text(120, 150, node.id);
-                var myNode = s.group(nodeCircle, nodeTitle);
-                
+                var image = s.image("resources/2363.png", 150, 150, 100, 100);
+
+                var myNode = s.group(nodeCircle, nodeTitle, image);
+
                 nodeCircle.attr({
                     fill: "#bada55",
                     stroke: "#000",
-                    strokeWidth: 5
+                    strokeWidth: 2
                 })
                 myNode.drag();
-                
-                nodeCircle.hover(function() {
-                    nodeCircle.animate({r: 120}, 100);}
-                ,
-            function() {
-                    nodeCircle.animate({r: 100}, 100);}
-                
-            );
+
+                nodeCircle.hover(
+                        function () {
+                            nodeCircle.animate({r: 120}, 100);
+                        },
+                        function () {
+                            nodeCircle.animate({r: 100}, 100);
+                        }
+                );
             });
         }
 
         /**
-	 * node render
-	 */
-	$scope.reorder = function() {
+         * node render
+         */
+        $scope.reorder = function () {
             console.info("reorder");
+            /*
+             var t = new Snap.Matrix()
+             t.translate(100, 100);
+             butterfly.transform(t); 
+             */
         }
 
         /**
-	 * load configuration
-	 */
-	$scope.loadModules = function(target, snap) {
-		/**
-		 * load configuration elements and blackboard
-		 */
-		$scope.modules.configuration = {};
-                $scope.modules.blackboard = Snap(snap);
-                
-                /**
-		 * load nodes
-		 */
-		modulesServices.getNodes({}, function(data) {
-			$scope.modules.nodes = data;
-                        $scope.render(true, $scope.modules.blackboard,$scope.modules.nodes);
-			/**
-			 * navigate to target
-			 */
-			$.mobile.navigate(target, {
-				info : "navigate to " + target
-			});
-		}, function(failure) {
-			/**
-			 * TODO : handle error message
-			 */
-		});
-	}
-	/**
-	 * load mongodb
-	 */
-	$scope.loadMongodb = function(target) {
-		/**
-		 * load mongodb elements
-		 */
-		$scope.modules.mongodb = {};
-		/**
-		 * loading mongodb collections then navigate to target
-		 */
-		modulesServices.getDbCollections({}, function(data) {
-			$scope.modules.mongodb.collections = data;
-			/**
-			 * navigate to target
-			 */
-			$.mobile.navigate(target, {
-				info : "navigate to " + target
-			});
-		}, function(failure) {
-			/**
-			 * TODO : handle error message
-			 */
-		});
-	}
-	/**
-	 * load collection in current scope
-	 * 
-	 * @param collection
-	 *            the selected collection
-	 */
-	$scope.loadCollection = function(collection, target) {
-		/**
-		 * loading mongodb collections then navigate to target
-		 */
-		modulesServices.getCollection({
-			'database' : collection.db,
-			'name' : collection.name,
-			'offset' : collection.count - 20,
-			'page' : 20
-		}, function(data) {
-			var columns = [];
-			for ( var column in data[0]) {
-				if (column.indexOf('$') == -1 && column.indexOf('toJSON') == -1) {
-					columns.push(column);
-				}
-			}
-			$scope.modules.mongodb.current = collection;
-			$scope.modules.mongodb.collection = data;
-			$scope.modules.mongodb.columns = columns;
-			$scope.modules.mongodb.offset = collection.count - 20;
-			$scope.modules.mongodb.page = 20;
+         * load configuration
+         */
+        $scope.loadModules = function (target, snap) {
+            /**
+             * load configuration elements and blackboard
+             */
+            $scope.modules.configuration = {};
+            $scope.modules.blackboard = Snap(snap);
 
-			/**
-			 * navigate to target
-			 */
-			$.mobile.navigate(target, {
-				info : "navigate to " + target
-			});
-		}, function(failure) {
-			/**
-			 * TODO : handle error message
-			 */
-		});
-	}
-	$scope.send = function(target, element) {
-		/**
-		 * loading clients
-		 */
-		modulesServices.send({
-			target : target,
-			message : element
-		}, function(data) {
-			console.log(data);
-		}, function(failure) {
-			/**
-			 * TODO : handle error message
-			 */
-		});
-	}
-	$scope.testJob = function(target) {
-		/**
-		 * loading clients
-		 */
-		modulesServices.testJob({
-			plugin : target.plugin,
-			job : target.job
-		}, function(data) {
-			console.log(data);
-		}, function(failure) {
-			/**
-			 * TODO : handle error message
-			 */
-		});
-	}
-	$scope.selectItem = function(item) {
-	}
-	$scope.selectGroup = function(item) {
-	}
-} ]);
+            /**
+             * load nodes
+             */
+            modulesServices.getNodes({}, function (data) {
+                $scope.modules.nodes = data;
+                $scope.render(true, $scope.modules.blackboard, $scope.modules.nodes);
+                /**
+                 * navigate to target
+                 */
+                $.mobile.navigate(target, {
+                    info: "navigate to " + target
+                });
+            }, function (failure) {
+                /**
+                 * TODO : handle error message
+                 */
+            });
+        }
+        /**
+         * load mongodb
+         */
+        $scope.loadMongodb = function (target) {
+            /**
+             * load mongodb elements
+             */
+            $scope.modules.mongodb = {};
+            /**
+             * loading mongodb collections then navigate to target
+             */
+            modulesServices.getDbCollections({}, function (data) {
+                $scope.modules.mongodb.collections = data;
+                /**
+                 * navigate to target
+                 */
+                $.mobile.navigate(target, {
+                    info: "navigate to " + target
+                });
+            }, function (failure) {
+                /**
+                 * TODO : handle error message
+                 */
+            });
+        }
+        /**
+         * load collection in current scope
+         * 
+         * @param collection
+         *            the selected collection
+         */
+        $scope.loadCollection = function (collection, target) {
+            /**
+             * loading mongodb collections then navigate to target
+             */
+            modulesServices.getCollection({
+                'database': collection.db,
+                'name': collection.name,
+                'offset': collection.count - 20,
+                'page': 20
+            }, function (data) {
+                var columns = [];
+                for (var column in data[0]) {
+                    if (column.indexOf('$') == -1 && column.indexOf('toJSON') == -1) {
+                        columns.push(column);
+                    }
+                }
+                $scope.modules.mongodb.current = collection;
+                $scope.modules.mongodb.collection = data;
+                $scope.modules.mongodb.columns = columns;
+                $scope.modules.mongodb.offset = collection.count - 20;
+                $scope.modules.mongodb.page = 20;
+
+                /**
+                 * navigate to target
+                 */
+                $.mobile.navigate(target, {
+                    info: "navigate to " + target
+                });
+            }, function (failure) {
+                /**
+                 * TODO : handle error message
+                 */
+            });
+        }
+        $scope.send = function (target, element) {
+            /**
+             * loading clients
+             */
+            modulesServices.send({
+                target: target,
+                message: element
+            }, function (data) {
+                console.log(data);
+            }, function (failure) {
+                /**
+                 * TODO : handle error message
+                 */
+            });
+        }
+        $scope.testJob = function (target) {
+            /**
+             * loading clients
+             */
+            modulesServices.testJob({
+                plugin: target.plugin,
+                job: target.job
+            }, function (data) {
+                console.log(data);
+            }, function (failure) {
+                /**
+                 * TODO : handle error message
+                 */
+            });
+        }
+        $scope.selectItem = function (item) {
+        }
+        $scope.selectGroup = function (item) {
+        }
+    }]);
